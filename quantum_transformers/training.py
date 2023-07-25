b657f5b6f4687685d70029daf5879159b75a9a25
Marçal Comajoan Cara <mcomajoancara@gmail.com>
2023-07-24 23:20:20 -0700

    Replace sklearn with torchmetrics
    sklearn was only used to compute AUROC using sklearn.metrics.roc_auc_score

--- quantum_transformers/training.py
+++ quantum_transformers/training.py
@@ -1,17 +1,20 @@
 import time
 
-import numpy as np
-from sklearn.metrics import roc_auc_score
 import torch
+import torchmetrics
 
-
-def train(model, train_dataloader, valid_dataloader, learning_rate, num_epochs, device, verbose=False) -> None:
+def train(model: torch.nn.Module, train_dataloader: torch.utils.data.DataLoader, valid_dataloader: torch.utils.data.DataLoader, num_classes: int,
+          learning_rate: float, num_epochs: int, device: torch.device, verbose: bool = False) -> None:
     """Trains the given model on the given dataloaders for the given parameters"""
     model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     criterion = torch.nn.CrossEntropyLoss()
 
     best_val_auc, best_epoch = 0.0, 0
+    if num_classes == 2:
+        auc_metric = torchmetrics.classification.BinaryAUROC()
+    else:
+        auc_metric = torchmetrics.classification.MulticlassAUROC(num_classes=num_classes)
     start_time = time.time()
     for epoch in range(num_epochs):
         step = 0
@@ -57,31 +60,23 @@ def train(model, train_dataloader, valid_dataloader, learning_rate, num_epochs, 
                 print(f"Epoch {epoch+1}/{num_epochs} ({time.time()-start_time:.2f}s): Step {step}, Loss = {loss.item():.4f}")
 
         model.eval()
-        y_true, y_score = [], []
         with torch.no_grad():
             val_loss = 0.0
-            val_steps = 0
-            val_correct = 0
             for inputs, labels in valid_dataloader:
                 inputs, labels = inputs.to(device), labels.long().to(device)
                 outputs = model(inputs)
                 val_loss += criterion(outputs, labels).item()
-                val_steps += 1
-                val_correct += (outputs.argmax(dim=1) == labels).sum().item()
-                y_true.append(labels.cpu().numpy())
-                y_score.append(outputs.softmax(dim=1).cpu().numpy())
+                if num_classes == 2:
+                    probabilities = torch.sigmoid(outputs[:, 1])
+                else:
+                    probabilities = torch.softmax(outputs, dim=1)
+                auc_metric.update(probabilities, labels)
 
-        y_true = np.concatenate(y_true)
-        y_score = np.concatenate(y_score)
-        if y_score.shape[1] == 2:  # binary classification
-            y_score = y_score[:, 1]
+        val_loss /= len(valid_dataloader)
+        val_auc = auc_metric.compute() * 100
 
-        val_loss /= val_steps
-        val_acc = val_correct / len(valid_dataloader.dataset)*100
-        val_auc = roc_auc_score(y_true, y_score, multi_class='ovr') * 100
         print(f"Epoch {epoch+1}/{num_epochs} ({time.time()-start_time:.2f}s):",
               f"Loss = {val_loss:.4f},",
-              f"Accuracy = {val_acc:.2f}%,",
               f"AUC = {val_auc:.2f}%")
         if val_auc > best_val_auc:
             best_val_auc = val_auc
