19c3a8183728535eb8493fde5e6c74bbf1128479
Marçal Comajoan Cara <mcomajoancara@gmail.com>
2023-08-20 11:28:59 -0700

    Remove unused remove_variables from utils

--- quantum_transformers/utils.py
+++ quantum_transformers/utils.py
@@ -1,14 +1,11 @@
-import gc
-
 import matplotlib.pyplot as plt
 import numpy as np
-import torch
 
 
 def plot_image(image, abs_log=False):
     """Plots an image with one subplot per channel"""
     num_channels = image.shape[2]
-    fig, axs = plt.subplots(1, num_channels, figsize=(num_channels*3, 3))
+    fig, axs = plt.subplots(1, num_channels, figsize=(num_channels * 3, 3))
     if num_channels == 1:
         axs = [axs]
     for i, ax in enumerate(axs):
@@ -17,11 +14,3 @@ def plot_image(image, abs_log=False):
         else:
             ax.imshow(image[:, :, i])
     plt.show()
-
-
-def delete_variables(*variables):
-    """Deletes variables from memory, empties the GPU cache, and runs garbage collection"""
-    for var in variables:
-        del var
-    torch.cuda.empty_cache()
-    gc.collect()
