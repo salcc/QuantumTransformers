226961da9bb5dd5e5376588316d80ca6728cf89d
Marçal Comajoan Cara <mcomajoancara@gmail.com>
2023-08-26 10:35:02 -0700

    Document training functions

--- quantum_transformers/training.py
+++ quantum_transformers/training.py
@@ -15,22 +15,35 @@ class TrainState(flax.training.train_state.TrainState):
 
 
 @jax.jit
-def train_step(state: TrainState, batch, dropout_key):
+def train_step(state: TrainState, inputs, labels, key):
+    """
+    Performs a single training step on the given batch of inputs and labels.
+
+    Args:
+        state: The current training state.
+        inputs: The batch of inputs.
+        labels: The batch of labels.
+        key: The random key to use.
+
+    Returns:
+        The updated training state.
+    """
+    key, dropout_key = jax.random.split(key=key)
     dropout_train_key = jax.random.fold_in(key=dropout_key, data=state.step)
 
     def loss_fn(params):
         logits = state.apply_fn(
             {'params': params},
-            x=batch['input'],
+            x=inputs,
             train=True,
             rngs={'dropout': dropout_train_key}
         )
         if logits.shape[1] <= 2:
             if logits.shape[1] == 2:
                 logits = logits[:, 1]
-            loss = optax.sigmoid_binary_cross_entropy(logits=logits, labels=batch['label']).mean()
+            loss = optax.sigmoid_binary_cross_entropy(logits=logits, labels=labels).mean()
         else:
-            loss = optax.softmax_cross_entropy_with_integer_labels(logits=logits, labels=batch['label']).mean()
+            loss = optax.softmax_cross_entropy_with_integer_labels(logits=logits, labels=labels).mean()
         # return loss, logits
         return loss
     # grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
@@ -42,36 +55,64 @@ def train_step(state: TrainState, batch, dropout_key):
 
 
 @jax.jit
-def eval_step(state: TrainState, batch):
+def eval_step(state: TrainState, inputs, labels):
+    """
+    Performs a single evaluation step on the given batch of inputs and labels.
+
+    Args:
+        state: The current training state.
+        inputs: The batch of inputs.
+        labels: The batch of labels.
+
+    Returns:
+        loss: The loss on the given batch.
+        logits: The logits on the given batch.
+    """
     logits = state.apply_fn(
         {'params': state.params},
-        x=batch['input'],
+        x=inputs,
         train=False,
         rngs={'dropout': state.key}
     )
     if logits.shape[1] <= 2:
         if logits.shape[1] == 2:
             logits = logits[:, 1]
-        loss = optax.sigmoid_binary_cross_entropy(logits=logits, labels=batch['label']).mean()
+        loss = optax.sigmoid_binary_cross_entropy(logits=logits, labels=labels).mean()
     else:
-        loss = optax.softmax_cross_entropy_with_integer_labels(logits=logits, labels=batch['label']).mean()
-    results = {'loss': loss, 'logits': logits, 'labels': batch['label']}
-    return results
+        loss = optax.softmax_cross_entropy_with_integer_labels(logits=logits, labels=labels).mean()
+    return loss, logits
 
 
 def train_and_evaluate(model: flax.linen.Module, train_dataloader, val_dataloader, num_classes: int,
                        num_epochs: int, learning_rate: float = 1e-3, seed: int = 42, verbose: bool = False) -> None:
-    """Trains the given model on the given dataloaders for the given parameters"""
+    """
+    Trains the given model on the given dataloaders for the given hyperparameters.
+
+    The progress and evaluation results are printed to stdout.
+
+    Args:
+        model: The model to train.
+        train_dataloader: The dataloader for the training set.
+        val_dataloader: The dataloader for the validation set.
+        num_classes: The number of classes.
+        num_epochs: The number of epochs to train for.
+        learning_rate: The learning rate to use.
+        seed: The seed to use for reproducibility.
+        verbose: Whether to print extra information.
+
+    Returns:
+        None
+    """
     root_key = jax.random.PRNGKey(seed=seed)
-    root_key, params_key, dropout_key = jax.random.split(key=root_key, num=3)
+    root_key, params_key, train_key = jax.random.split(key=root_key, num=3)
 
     dummy_batch = next(iter(train_dataloader))[0]
     input_shape = dummy_batch[0].shape
     input_dtype = dummy_batch[0].dtype
     batch_size = len(dummy_batch)
-    x = jnp.zeros(shape=(batch_size,) + tuple(input_shape), dtype=input_dtype)  # Dummy input
+    inputs_batch = jnp.zeros(shape=(batch_size,) + tuple(input_shape), dtype=input_dtype)  # Dummy input
 
-    variables = model.init(params_key, x, train=False)
+    variables = model.init(params_key, inputs_batch, train=False)
 
     if verbose:
         print(jax.tree_map(lambda x: x.shape, variables))
@@ -81,7 +122,7 @@ def train_and_evaluate(model: flax.linen.Module, train_dataloader, val_dataloade
     state = TrainState.create(
         apply_fn=model.apply,
         params=variables['params'],
-        key=dropout_key,
+        key=train_key,
         tx=optimizer
     )
 
@@ -89,22 +130,20 @@ def train_and_evaluate(model: flax.linen.Module, train_dataloader, val_dataloade
     start_time = time.time()
     for epoch in range(num_epochs):
         with tqdm(total=len(train_dataloader), desc=f"Epoch {epoch+1:3}/{num_epochs}", unit="batch", bar_format='{l_bar}{bar:10}{r_bar}{bar:-10b}') as progress_bar:
-            for x, y in train_dataloader:
+            for inputs_batch, labels_batch in train_dataloader:
                 step_start_time = time.time()
-                batch = {'input': x, 'label': y}
-                state = train_step(state, batch, dropout_key)
+                state = train_step(state, inputs_batch, labels_batch, train_key)
                 progress_bar.update(1)
                 if verbose:
                     print(f" Step {state.step+1}/{len(train_dataloader)}: {time.time()-step_start_time:.2f}s")
 
             logits, labels = [], []
             val_loss = 0.0
-            for x, y in val_dataloader:
-                batch = {'input': x, 'label': y}
-                results = eval_step(state, batch)
-                logits.append(results['logits'])
-                labels.append(results['labels'])
-                val_loss += results['loss']
+            for inputs_batch, labels_batch in val_dataloader:
+                loss_batch, logits_batch = eval_step(state, inputs_batch, labels_batch)
+                logits.append(logits_batch)
+                labels.append(labels_batch)
+                val_loss += loss_batch
             val_loss /= len(val_dataloader)
             logits = jnp.concatenate(logits)
             y_true = jnp.concatenate(labels)
